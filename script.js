76476a353daffaa40e1cb1046eae9f07303bf860
Hunter Wallace <32723509+Hjwallace@users.noreply.github.com>
2020-10-29 23:17:01 -0400

    State Selection Capability

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 
 
-$.getJSON("https://api.covidtracking.com/v1/states/in/current.json", 
-function(data){
-
-    //console.log(data);
-
-    var state = data.state;
-    var lastupdate = data.lastUpdateEt;
-    var positive = data.positive - data.recovered - data.death;
-    var incPos = data.positiveIncrease;
-    var totalTests = data.totalTestResults;
-
-    var currHospital = data.hospitalizedCurrently;
-    var incHospital = data.hospitalizedIncrease;
-
-    $(".state").append("<b>State: </b>" + state);
-    $(".lastupdate").append("<b>Last Updated: </b>" + lastupdate);
-    $(".positive").append("<b>Positive: </b>" + positive.toLocaleString());
-    $(".incPos").append("<b>Latest New Cases in a Day: </b>" + incPos.toLocaleString());
-    $(".totalTests").append("<b>Total Tests Conducted: </b>" + totalTests.toLocaleString());
-
-    $(".currHospital").append("<b>Current Hospital Cases: </b>" + currHospital.toLocaleString());
-    $(".incHospital").append("<b>Latest New Hospital Cases: </b>" + incHospital.toLocaleString());
-
-
-
-})
 
 
 
